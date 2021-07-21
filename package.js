33b3515c3294b980fc69ed2347fe35f80ee2a38d
Brent Abrahams <brent_abrahams@yahoo.com>
2021-07-22 00:15:01 +0800

    Updated for 2.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'constellation:shorten-collection-names',
-  version: '0.4.7',
+  version: '0.4.11',
   summary: 'Makes long collection names visible in Constellation',
   git: 'https://github.com/JackAdams/constellation-shorten-collection-names.git',
   documentation: 'README.md',
@@ -8,14 +8,14 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom(['1.8.2', '2.3']);
 
-  api.use(['templating','reactive-dict','tracker'], 'client');
-  api.use('constellation:console@1.4.7', 'client');
+  api.use(['templating@1.3.2', 'reactive-dict', 'tracker'], 'client');
+  api.use('constellation:console@1.4.11', 'client');
 
-  api.addFiles('shorten-collection-names.css','client');
-  api.addFiles('shorten-collection-names.html','client');
-  api.addFiles('shorten-collection-names.js','client');
+  api.addFiles('shorten-collection-names.css', 'client');
+  api.addFiles('shorten-collection-names.html', 'client');
+  api.addFiles('shorten-collection-names.js', 'client');
   
   api.imply('constellation:console');
 });
